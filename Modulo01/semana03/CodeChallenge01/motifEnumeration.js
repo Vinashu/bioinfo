d057398e603e97d9e6528db4619e5d22f42d48ba
Rogerio de Leon Pereira <vinashu@gmail.com>
2016-11-25 13:32:59 -0200

    Resolvido motifEnumeration

--- Modulo01/semana03/CodeChallenge01/motifEnumeration.js
+++ Modulo01/semana03/CodeChallenge01/motifEnumeration.js
@@ -7,123 +7,65 @@ var dna = [
     'GAAAATT'
 ];
 var output = 'ATA ATT GTT TTT';
-
 var answer = motifEnumeration(dna, k, d); 
 console.log("Outuput = " + output);
 console.log("Answer = " + answer);
 console.log(answer === output);
 
+k = 5;
+d = 2;
+var dna = [
+    'TCTGAGCTTGCGTTATTTTTAGACC',
+    'GTTTGACGGGAACCCGACGCCTATA',
+    'TTTTAGATTTCCTCAGTCCACTATA',
+    'CTTACAATTTCGTTATTTATCTAAT',
+    'CAGTAGGAATAGCCACTTTGTTGTA',
+    'AAATCCATTAAGGAAAGACGACCGT'
+];
+
+output = "AAACT AAATC AACAC AACAT AACCT AACTA AACTC AACTG AACTT AAGAA AAGCT AAGGT AAGTC AATAC AATAT AATCC AATCT AATGC AATTC AATTG ACAAC ACACA ACACC ACACG ACACT ACAGA ACAGC ACATC ACATG ACCAT ACCCT ACCGT ACCTA ACCTC ACCTG ACCTT ACGAC ACGAG ACGAT ACGCT ACGGT ACGTC ACGTT ACTAA ACTAG ACTAT ACTCA ACTCC ACTCG ACTCT ACTGA ACTGC ACTGT ACTTA ACTTC ACTTT AGAAA AGAAC AGAAG AGAAT AGACA AGACT AGATA AGATC AGCAT AGCCA AGCGT AGCTA AGCTC AGCTG AGCTT AGGAT AGGTA AGGTC AGTAA AGTAC AGTAT AGTCC AGTCG AGTCT AGTGA AGTTG ATAAA ATAAC ATACA ATACC ATAGA ATATA ATATC ATATG ATATT ATCAG ATCCC ATCCG ATCCT ATCGA ATCGC ATCTA ATCTC ATCTG ATGAC ATGAT ATGCA ATGCC ATGGA ATGGC ATGTA ATGTC ATTAA ATTAC ATTAG ATTAT ATTCA ATTCC ATTCG ATTGA ATTGC ATTGG ATTGT ATTTA ATTTC ATTTG ATTTT CAAAG CAACC CAACT CAAGA CAAGC CAATA CAATT CACAC CACAG CACCT CACGT CACTA CACTT CAGAA CAGAC CAGAT CAGGT CAGTA CAGTC CATAA CATAC CATAG CATAT CATCC CATCT CATGA CATGT CATTA CATTG CATTT CCAAG CCATA CCATG CCATT CCCGT CCCTA CCCTT CCGAA CCGAC CCGAT CCGCT CCGGT CCGTA CCGTC CCGTG CCGTT CCTAC CCTAT CCTCA CCTCC CCTTA CCTTC CCTTG CCTTT CGAAA CGAAG CGACA CGACT CGAGT CGATA CGATG CGATT CGCAA CGCAT CGCCA CGCGA CGCTA CGCTC CGCTT CGGAC CGGAT CGGCA CGGTA CGGTC CGGTT CGTAA CGTAC CGTCA CGTCG CGTCT CGTTA CGTTT CTAAC CTAAG CTAAT CTACA CTACC CTACG CTACT CTAGA CTAGC CTAGG CTAGT CTATA CTATC CTATG CTATT CTCAT CTCCG CTCGT CTCTA CTCTT CTGAA CTGAG CTGCA CTGCC CTGTA CTGTT CTTAA CTTAC CTTAG CTTAT CTTCA CTTGA CTTTA CTTTC CTTTG CTTTT GAAAT GAACA GAACT GAAGT GAATG GAATT GACAC GACAT GACCA GACCT GACGT GACTT GAGAA GAGAT GAGCT GATAA GATAC GATAG GATAT GATCA GATCC GATCG GATCT GATGT GATTA GATTC GATTG GATTT GCAAT GCACT GCATC GCATT GCCAT GCCGT GCCTA GCCTT GCGAT GCGGT GCGTC GCGTT GCTAA GCTAC GCTAG GCTAT GCTGA GCTGT GCTTA GCTTT GGAAT GGACA GGATA GGATC GGATT GGCTA GGGAT GGTAC GGTAG GGTAT GGTCA GGTCG GGTTA GTAAA GTAAG GTACA GTACC GTACG GTAGA GTATA GTATC GTATG GTATT GTCAA GTCAG GTCCG GTCCT GTCGA GTCGC GTCGT GTCTA GTCTG GTGAA GTGAG GTGCA GTGCG GTTAA GTTAC GTTAG GTTAT GTTCA GTTCC GTTCG GTTGA GTTTA TAAAC TAAAG TAACA TAACC TAACT TAAGA TAAGC TAATA TAATC TACAC TACAG TACCC TACCG TACCT TACGA TACGC TACGT TACTA TACTC TACTG TAGAA TAGAC TAGAG TAGAT TAGCC TAGCG TAGGA TAGTC TATAA TATAC TATAT TATCA TATCC TATCG TATGA TATGC TATGG TATGT TATTA TATTG TCAAC TCAAT TCACC TCACG TCACT TCAGA TCATA TCATG TCCAA TCCAC TCCAG TCCAT TCCCA TCCCT TCCGA TCCGC TCCGT TCCTA TCCTG TCCTT TCGAA TCGAC TCGAT TCGCC TCGCT TCGGA TCGGC TCGGG TCGGT TCGTC TCTAC TCTAG TCTAT TCTCC TCTCT TCTGG TCTGT TCTTA TCTTT TGAAA TGAAC TGAAT TGACA TGACC TGACT TGAGA TGAGC TGAGT TGATA TGATC TGATG TGATT TGCAA TGCAC TGCAG TGCAT TGCCA TGCCG TGCCT TGCGA TGCGT TGCTT TGGAA TGGAT TGGTA TGTAA TGTAG TGTAT TGTCC TGTCG TGTGG TGTTA TTAAA TTAAC TTAAG TTAAT TTACA TTACC TTACG TTACT TTAGA TTAGC TTAGG TTAGT TTATA TTATC TTATG TTATT TTCAA TTCAC TTCAT TTCCA TTCCC TTCCT TTCGA TTCGG TTCGT TTCTA TTCTG TTGAA TTGAC TTGAG TTGAT TTGCA TTGCG TTGGA TTGGG TTGTG TTTAA TTTAC TTTAG TTTAT TTTCA TTTCC TTTCG TTTGA TTTGG TTTTA TTTTG";
+
+answer = motifEnumeration(dna, k, d); 
+console.log("Outuput = " + output);
+console.log("Answer = " + answer);
+console.log(answer === output);
+
+
 function motifEnumeration(dna, k, d) {
-    var max = Math.pow(4,k)-1;
-    var index;
-    var tam;
-    var frequencyArray = [];
-    var frequentPatterns = [];    
-    var neighborhood = [];
     var patterns = [];
-    var pattern;
-    for (var i = 0; i <= max; i++){
-        frequencyArray[i] = 0;
-    }    
+    var neighborhood = [];
+    var frequentPatterns = [];
+    var frequencyArray = [];
+    var max = Math.pow(4,k)-1; 
+    var index;    
+    var pattern;   
+    var tam = dna.length;
     dna.forEach(function(text){
-        console.log(text);
-        tam = text.length - k;
-        for (var i = 0; i <= tam; i++){            
-            neighborhood = neighbors(text.substr(i, k), d);
-            //neighborhood = removeDuplicate(neighborhood);           
-            if (Array.isArray(neighborhood)){
-                neighborhood.forEach(function(pattern){
-                    if(patternCount(text, pattern) > 0){
-                        index = patternToNumber(pattern);
-                        frequencyArray[index] +=  1;
-                        console.log(pattern);
-                    }
-                });
-            } else {
-                if(patternCount(text, pattern) > 0){
-                    index = patternToNumber(pattern);
-                    frequencyArray[index] +=  1;
-                    console.log(pattern);                    
-                }            
-            }      
-        }
+        neighborhood = [];
+        patterns = findPatterns(text,k);
+        patterns.forEach(function(pattern){
+            neighborhood = neighborhood.concat(neighbors(pattern,d));
+        });   
+        neighborhood = removeDuplicate(neighborhood);
+        frequentPatterns.push(neighborhood);
     });
     for (var i = 0; i <= max; i++){
-        if (frequencyArray[i] == dna.length) {
-            pattern = numberToPattern(i, k);
-            frequentPatterns.push(pattern);
-        }
-    }
-    //console.log(frequencyArray);
-    return frequentPatterns;
-}
-
-function patternCount(text, pattern) {
-    var count = 0;
-    var max = text.length - pattern.length;
-    var pedaco;
-    for (var i = 0; i <= max; i++ ) {
-        pedaco = text.substring(i, i + pattern.length);
-        if(pedaco === pattern) {
-            count++;
-        }
-    } 
-    return count;
-}
-
-function removeDuplicate(conjunto) {
-	var novo = []; 
-    var max = conjunto.length;
-	for(var i = 0; i < max; i++) {
-		if (novo.indexOf(conjunto[i]) == -1) {
-            novo.push(conjunto[i]);
-        }
-	}
-	return novo;
-}        
-
-
-function frequentWordsWithMismatches(text, k, d) {
-    var frequentPatterns = [];
-    var close = [];
-    var max = Math.pow(4,k)-1;
-    var tam = text.length - k;    
-    var neighborhood = [];
-    var frequencyArray = [];
-    var index;
-    for (var i = 0; i <= max; i++){
-        close[i] = 0;
         frequencyArray[i] = 0;
     }
-    for (var i = 0; i <= tam; i++){
-        neighborhood = neighbors(text.substr(i, k), d);
-        if (Array.isArray(neighborhood)){
-            neighborhood.forEach(function(pattern){
-                index = patternToNumber(pattern);
-                close[index] =  1;
-            });
-        } else {
-            index = patternToNumber(neighborhood);
-            close[index] =  1;            
-        }      
-    }
-    var pattern;
+    frequentPatterns.forEach(function(patterns){
+        patterns.forEach(function(pattern){
+            index = patternToNumber(pattern);
+            frequencyArray[index] += 1;
+        });  
+    });         
+    frequentPatterns = [];
     for (var i = 0; i <= max; i++){
-        if (close[i] == 1) {
-            pattern = numberToPattern(i, k);
-            frequencyArray[i] = approximatePatternCount(pattern, text, d) + approximatePatternCount(reverse(pattern), text, d);
-            index = patternToNumber(reverse(pattern));
-            frequencyArray[index] = approximatePatternCount(pattern, text, d) + approximatePatternCount(reverse(pattern), text, d);           
-        }
-    }
-    var maxCount = Math.max.apply(null, frequencyArray);
-    for (var i = 0; i <= max; i++){
-        if (frequencyArray[i] == maxCount) {
-            pattern = numberToPattern(i, k);
+        if(frequencyArray[i] >= tam){
+            pattern = numberToPattern(i,k); 
             frequentPatterns.push(pattern);
         }
     }
-    return frequentPatterns; 
+    return frequentPatterns.join(" ");
 }
 
 function neighbors(pattern,d){
@@ -156,6 +98,28 @@ function hammingDistance(text1, text2) {
         }
     } 
     return (distance);
+}
+
+function findPatterns(text, k){
+    var patterns = [];
+    var max = text.length - k;
+    var pattern;
+    for(var i = 0; i <= max; i++){
+        pattern = text.substring(i, i+k);
+        patterns.push(pattern);
+    }
+    return patterns;
+}  
+
+function removeDuplicate(conjunto) {
+	var novo = []; 
+    var max = conjunto.length;
+	for(var i = 0; i < max; i++) {
+		if (novo.indexOf(conjunto[i]) == -1) {
+            novo.push(conjunto[i]);         
+        }
+	}   
+	return novo;
 }
 
 function patternToNumber(pattern){
@@ -205,40 +169,4 @@ function numberToSymbol(number){
         case 3:
             return "T";
     }
-}
-
-function approximatePatternCount(pattern, text, mm){
-    var p = pattern.length;
-    var max = text.length - p;
-    distances = 0;
-    for(var i = 0; i <= max; i++){
-        if(hammingDistance(pattern, text.substr(i,p)) <= mm){
-            distances++;
-        }
-    }
-    return distances;
-}
-
-function hammingDistance(text1, text2) {
-    var max = text1.length;
-    var distance = 0;  
-    for (var i = 0; i < max; i++ ) {
-        if (text1.charAt(i) != text2.charAt(i)) {
-            distance++;
-        }
-    } 
-    return (distance);
-}
-
-function reverse(pattern) {
-    rpattern = pattern.split('').reverse().join('');  
-    rpattern = rpattern.replace(/A/g,"0");       
-    rpattern = rpattern.replace(/T/g,"1");
-    rpattern = rpattern.replace(/C/g,"2");
-    rpattern = rpattern.replace(/G/g,"3");     
-    rpattern = rpattern.replace(/0/g,"T");
-    rpattern = rpattern.replace(/1/g,"A");
-    rpattern = rpattern.replace(/2/g,"G");
-    rpattern = rpattern.replace(/3/g,"C");    
-    return rpattern;
 }
